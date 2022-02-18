f6016b0efa3bf0f90a659826692500383237bdac
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-02-18 13:45:46 -0800

    Updated Supers File For POST Functionality

--- supers/views.py
+++ supers/views.py
@@ -8,61 +8,65 @@ from .models import Supers
 from supers import serializers
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
+    if request.method == 'GET':
+        supers = Supers.objects.all()
+        serializer = SupersSerializer(supers, many=True)
+        return Response(serializer.data)
 
-    supers = Supers.objects.all()
+    elif request.method == 'POST':
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    serializer = SupersSerializer(supers, many=True)
+        ##########################################################
 
-    return Response(serializer.data)
+        # @api_view(['GET', 'POST'])
+        # def supers_list(request):
+        #     if request.method == 'GET':
+        #         super = Supers.objects.all()
+        #         # data_visualization = [item for item in queryset]  # added for debugging
+        #         serializer = SupersSerializer(super, many=True)
+        #         return Response(serializer.data)
 
-# @api_view(['GET', 'POST'])
-# def supers_list(request):
-#     if request.method == 'GET':
-#         super = Supers.objects.all()
-#         # data_visualization = [item for item in queryset]  # added for debugging
-#         serializer = SupersSerializer(super, many=True)
-#         return Response(serializer.data)
+        #     elif request.method == 'POST':
+        #         serializer = SupersSerializer(data=request.data)
+        #         serializer.is_valid(raise_exception=True)
+        #         serializer.save()
+        #         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-#     elif request.method == 'POST':
-#         serializer = SupersSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # @api_view(['GET', 'PUT', 'DELETE'])
+        # def supers_detail(request, pk):
+        #     super = get_object_or_404(Supers, pk=pk)
+        #     if request.method == 'GET':
+        #         serializer = SupersSerializer(super)
+        #         return Response(serializer.data)
+        #     elif request.method == 'PUT':
+        #         serializer = SupersSerializer(super, data=request.data)
+        #         serializer.is_valid(raise_exception=True)
+        #         serializer.save()
+        #         return Response(serializer.data)
+        #     elif request.method == 'DELETE':
+        #         super.delete()
+        #         return Response(status=status.HTTP_204_NO_CONTENT)
 
+        # @api_view(['GET'])
+        # def supers_list(request):
 
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def supers_detail(request, pk):
-#     super = get_object_or_404(Supers, pk=pk)
-#     if request.method == 'GET':
-#         serializer = SupersSerializer(super)
-#         return Response(serializer.data)
-#     elif request.method == 'PUT':
-#         serializer = SupersSerializer(super, data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data)
-#     elif request.method == 'DELETE':
-#         super.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+        #     super_type_param = request.query_params.get('super_type')
+        #     sort_param = request.query_params.get('sort')
 
+        #     supers = SuperType.objects.all()
 
-# @api_view(['GET'])
-# def supers_list(request):
+        #     # Truthy: if contains value then it is True
+        #     # if does not contain value then it is False
+        #     if super_type_param:
+        #         supers = supers.filter(supers_name=super_type_param)
 
-#     super_type_param = request.query_params.get('super_type')
-#     sort_param = request.query_params.get('sort')
+        #     if sort_param:
+        #         supers = supers.order_by(sort_param)
 
-#     supers = SuperType.objects.all()
-
-#     # Truthy: if contains value then it is True
-#     # if does not contain value then it is False
-#     if super_type_param:
-#         supers = supers.filter(supers_name=super_type_param)
-
-#     if sort_param:
-#         supers = supers.order_by(sort_param)
-
-#     serializer = SupersSerializer(supers, many=True)
-#     return Response(serializer.data)
+        #     serializer = SupersSerializer(supers, many=True)
+        #     return Response(serializer.data)
