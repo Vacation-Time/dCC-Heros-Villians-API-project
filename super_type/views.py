0697600d5e59cba4bc8df966fbc079fa3118bb62
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-02-21 14:45:44 -0800

    Created Working GET/POST/PUT/DEL Call Requests for Super_type App

--- super_type/views.py
+++ super_type/views.py
@@ -9,21 +9,26 @@ from .models import SuperType
 from super_type import serializers
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def super_type_list(request):
+    if request.method == 'GET':
+        super_type = SuperType.objects.all()
+        # data_visualization = [item for item in supers]  # for debugging
+        serializer = SuperTypeSerializer(super_type, many=True)
+        return Response(serializer.data)
 
-    super_types = SuperType.objects.all()
-    # data_visualization = [item for item in super_types]  # for debugging
-    serializer = SuperTypeSerializer(super_types, many=True)
-
-    return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperTypeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_type_detail(request, pk):
     super_type = get_object_or_404(SuperType, pk=pk)
     if request.method == 'GET':
-        data_visualization = [item for item in super_type]  # for debugging
+        # data_visualization = [item for item in super_type]  # for debugging
         serializer = SuperTypeSerializer(super_type)
         return Response(serializer.data)
     elif request.method == 'PUT':
